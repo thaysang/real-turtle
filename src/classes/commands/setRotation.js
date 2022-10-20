19869d4e1a43ee75457310e54fb47ef2b9ea4209
sangltv <sangltv@proton.me>
2022-10-20 00:32:13 +0000

    setRotation

---
+++ src/classes/commands/setRotation.js
@@ -0,0 +1,26 @@
+import Command from "../constructors/drawingCommand";
+
+export default class SetRotationCommand extends Command {
+  static params = {
+    deg: new Number(),
+  };
+
+  constructor(options) {
+    super(options);
+  }
+
+  estimate(main) {
+    return {
+      requiredTime: 0,
+    };
+  }
+
+  prepare(main) {}
+
+  async execute(progress, ctx) {
+    return new Promise((resolve) => {
+      this.state.setRotation(this.options.deg);
+      resolve();
+    });
+  }
+}
--- src/definitions/commandNames.js
+++ src/definitions/commandNames.js
@@ -34,8 +34,8 @@ export default [
   "setPosition",
   "resetPosition",
   "setInitialPosition",
+  "setRotation",
   "setInitialRotation",
-
   "beginPath",
   "closePath",
   "fill",
