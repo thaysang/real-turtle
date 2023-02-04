2ee845872c2bf378b18ea17e7c761d20ebcfc506
sangltv <113423052+sangltv@users.noreply.github.com>
2023-02-04 01:55:59 +0000

    rebuiild3

--- src/classes/commands/goto.js
+++ src/classes/commands/goto.js
@@ -8,29 +8,27 @@ export default class MoveCommand extends Command {
   }
 
   estimate(main) {
+    let steps = Math.sqrt((this.options.x - this.state.position.x)**2 + (this.options.y - this.state.position.y)**2)
     return {
       requiredTime:
-        (1 - this.main.state.speed) * Math.abs(this.steps) * 5,
+      (1 - this.main.state.speed) * Math.abs(steps) * 5,
     };
   }
 
   prepare(main) {
-    this.moveX = this.options.x - this.initialState.position.x 
-    this.moveY = this.options.y - this.initialState.position.y
-    this.steps = Math.sqrt(this.moveX**2 + this.moveY**2)
-    this.deg = 180*Math.atan2(this.moveY, this.moveX)/Math.PI + 90;
+
   }
 
   async execute(progress, ctx) {
     return new Promise((resolve) => {
-      let xNow = this.options.x + this.initialState.position.x * (1 - progress);
-      let yNow = this.options.y + this.initialState.position.y * (1 - progress);
+      let moveX = this.options.x - this.state.position.x 
+      let moveY = this.options.y - this.state.position.y
+      this.state.setRotation(180*Math.atan2(moveY, moveX)/Math.PI - 90);
       if (!this.state.pathActive) {
         ctx.beginPath();
         ctx.moveTo(this.initialState.position.x, this.initialState.position.y);
       }
-      // this.state.setRotation(this.angle)
-      ctx.lineTo(xNow, yNow);
+      ctx.lineTo(this.options.x,this.options.y)
 
       if (this.state.strokeActive) {
         ctx.stroke();
@@ -40,8 +38,8 @@ export default class MoveCommand extends Command {
         ctx.closePath();
       }
 
-      this.state.setPosition(xNow, yNow);
-      this.state.setRotation(this.deg);
+      this.state.setPosition(this.options.x,this.options.y);
+      
       resolve();
     });
   }
